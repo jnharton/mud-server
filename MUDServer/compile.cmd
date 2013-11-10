fc74f59b39e8c8015d688c07beff2bef9d22cc88
jnharton <jnharton@gmail.com>
2013-11-09 19:09:17 -0500

    dropped compile.cmd from repository, added build.xml

--- MUDServer/compile.cmd
+++
@@ -1,5 +0,0 @@
-javac -Xlint:unchecked -cp .;classes;C:\Users\Jeremy\Desktop\junit4.11\junit-4.11.jar -d classes src/mud/*.java src/mud/colors/*.java src/mud/miscellaneous/*.java src/mud/commands/*.java src/mud/interfaces/*.java src/mud/magic/*.java src/mud/net/*.java src/mud/objects/*.java src/mud/objects/items/*.java src/mud/objects/things/*.java src/mud/protocols/*.java src/mud/quest/*.java src/mud/utils/*.java src/mud/weather/*.java test/mud/net/*.java 
-
-REM java -cp classes;C:\Users\Owner\Desktop\junit4.11\junit-4.11.jar org.junit.runner.JUnitCore mud.net.ClientTest
-
-REM java -cp classes;C:\Users\Jeremy\Documents\GitHub\junit4.11\junit-4.11.jar mud.MUDServer 7777
